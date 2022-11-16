8894c8696b7bf7034ca8fd70d3cfb6ee48e9128c
Kenanimok <sutha.w@touchtechnologeis.co.th>
2022-11-16 16:22:40 +0700

    fix login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import logo from "./logo.svg";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+import Admin from "./pages/adminpage/admin";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/admin" element={<Admin />} />
+    </Routes>
+  );
 }
 
 export default App;
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,42 @@
 import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return <div>login</div>;
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:4000/api/login", user);
+      console.log(res);
+      navigate("/admin");
+    } catch (err) {
+      alert("err");
+    }
+  };
+
+  const handleChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        username: <input type="text" name="username" onChange={handleChange} />
+        password: <input type="text" name="password" onChange={handleChange} />
+        <button>sumit</button>
+      </form>
+    </>
+  );
 };
 
 export default Login;
